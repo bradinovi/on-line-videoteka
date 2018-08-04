e5fa8ee9c7140e7b927873affa97fd8c2b19f965
Borna Radinović <bradinovi@foi.hr>
2018-08-04 22:41:30 +0200

    Dodana komponenta za registraciju

--- on-line-videoteka/src/app/app-routing.module.ts
+++ on-line-videoteka/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 // my components imported
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
--- on-line-videoteka/src/app/app.module.ts
+++ on-line-videoteka/src/app/app.module.ts
@@ -11,10 +11,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent, NavbarComponent, LoginComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
