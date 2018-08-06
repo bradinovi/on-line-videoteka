5432fde99f4ed8fc432bb45f4b4d2b73b4ed6dd1
Borna Radinović <bradinovi@foi.hr>
2018-08-06 13:52:32 +0200

    Dodana komponenta za prikaz kartice filma
    Komponenta služi za prikaz filmova prilikom pretrage i na početnoj stranici.

---
+++ on-line-videoteka/src/app/moviecard/moviecard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-moviecard',
+  templateUrl: './moviecard.component.html',
+  styleUrls: ['./moviecard.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MoviecardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
