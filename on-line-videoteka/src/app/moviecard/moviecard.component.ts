40c27f5b67ab83ba32daf2aca87fdf034426b841
Borna Radinović <bradinovi@foi.hr>
2018-08-06 16:35:53 +0200

    Movie card data atributi dodani + card style update

--- on-line-videoteka/src/app/moviecard/moviecard.component.ts
+++ on-line-videoteka/src/app/moviecard/moviecard.component.ts
@@ -8,7 +8,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class MoviecardComponent implements OnInit {
 
-  constructor() { }
+  movieTitle = '';
+  movieGenres = '';
+  moviePrice = '';
+  moviePosterPath =
+  'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SY1000_CR0,0,674,1000_AL_.jpg';
+
+  constructor() {
+    // test data
+    this.movieTitle = 'Avengers: Infinity War' + '(' + '2018' + ')';
+    this.movieGenres = 'Action, Adventure, Fantasy';
+    this.moviePrice = '3';
+
+  }
 
   ngOnInit() {
   }
