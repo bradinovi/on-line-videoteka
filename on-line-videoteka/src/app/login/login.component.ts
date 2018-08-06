3ed43cf776724064ae7ff6d1dc18f1b26cf155a6
Borna Radinović <bradinovi@foi.hr>
2018-08-06 13:49:54 +0200

    Login komponenta dodana data logika
    data logika login komponenta + clean package.json + index.html + package-lock.json

--- on-line-videoteka/src/app/login/login.component.ts
+++ on-line-videoteka/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../node_modules/@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userEmail: string;
+  userPassword: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onLogIn(logInForm: NgForm) {
+    if (logInForm.invalid) {
+      return;
+    }
+    this.userEmail = logInForm.value.email;
+    this.userPassword = logInForm.value.password;
+
+  }
 }
