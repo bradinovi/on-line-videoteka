c5ffa8762d9d7f5583274718f77b7bbd0e6beed6
Borna Radinović <bradinovi@foi.hr>
2018-08-12 20:18:00 +0200

    Angular app data models Rent,Role

---
+++ on-line-videoteka/src/app/models/rent.model.ts
@@ -0,0 +1,7 @@
+export interface Rent {
+  id: string;
+  user: string;
+  movie: string;
+  duration: number;
+  hasLiked: boolean;
+}
---
+++ on-line-videoteka/src/app/models/role.model.ts
@@ -0,0 +1,6 @@
+export interface Role {
+  id: string;
+  movie: string;
+  actor: string;
+  name: string;
+}
